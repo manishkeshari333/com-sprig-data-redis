1d6516c68d428ceebba8129ac5dd897d104f2d64
Manish Keshari <manishkeshari595@gmail.com>
2024-09-14 14:01:58 +0530

    second commit

--- com-sprig-data-redis-sql/src/main/java/com/sprig/data/redis/sql/repo/TaskRepository.java
+++ com-sprig-data-redis-sql/src/main/java/com/sprig/data/redis/sql/repo/TaskRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.sprig.data.redis.sql.entity.Task;
 
+@repository
 public interface TaskRepository extends MongoRepository<Task, String> {
 	// Custom query methods can be defined here
 	Task findByTitle(String title);
